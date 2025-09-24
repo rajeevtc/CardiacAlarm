ddbc677a9228d3f12ed7a6a8d35fe38bbfba8a02
Rajeev TC <rajeev@hivelocity.co.jp>
2025-09-24 23:28:27 +0530

    Initial commit

---
+++ CardiacAlarm Watch App/App/CardiacAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardiacAlarmApp.swift
+//  CardiacAlarm Watch App
+//
+//  Created by Rajeev TC on 2025/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CardiacAlarm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HeartRateDisplayScreen()
+        }
+    }
+}
